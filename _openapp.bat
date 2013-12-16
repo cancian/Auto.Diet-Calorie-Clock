47a1b671c495b1fdb05bde70c437274660ee3c5d
André Cancian <cancian@ateus.net>
2013-12-16 10:18:10 -0200

    1.0.5 - shortcuts

--- _openapp.bat
+++
@@ -1,7 +0,0 @@
-path "C:\Program Files (x86)\Adobe\Adobe Dreamweaver CS6"
-dreamweaver
-dreamweaver index.html
-dreamweaver js\index.js
-dreamweaver js\diary.js
-dreamweaver js\lang.js
-dreamweaver css\index.css
