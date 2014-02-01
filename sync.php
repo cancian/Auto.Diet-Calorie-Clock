bf3c084b4e42722bfddb080afe868b7f8fcb1aa3
André Cancian <cancian@ateus.net>
2014-02-01 09:45:39 -0200

    1.1.5 ~ first stable sync

--- sync.php
+++ sync.php
@@ -1,20 +1,20 @@
 <?php ######################
 ## MYLIVEDIET PHP BACKEND ##
 ############################
-//if($_GET && $_POST)   { die(); }
-//if(!$_GET && !$_POST) { die(); }
+if($_GET && $_POST)   { die(); }
+if(!$_GET && !$_POST) { die(); }
 
-//if(!$_POST) {
+if(!$_POST) {
 	if($_GET && is_numeric($_GET['uid']))  { 
 		if(file_exists('userdata/'.$_GET['uid'].'.sql')) {
 			$userdata = file_get_contents('userdata/'.$_GET['uid'].'.sql');
 			print($userdata);
 		}
 		die();
-	} //else {
-	//	die();
-	//}
-//} else {
+	} else {
+		die();
+	}
+} else {
 	if($_POST && !is_numeric($_POST['uid'])) { die(); }
 	if($_POST && !$_POST['sql']) 			 { die(); }
 	if($_GET) 								 { die(); }
@@ -25,5 +25,5 @@
 	$file_handle = fopen('userdata/'.$userid.'.sql', 'w');
 	fwrite($file_handle, $pushed_data);
 	fclose($file_handle);
-//}
+}
 ?>
