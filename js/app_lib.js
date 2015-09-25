9e901a51904a113bc07c2f9a6dfab08a70c13615
André Cancian <cancian@ateus.net>
2015-09-25 09:00:46 -0300

    1.9.9 ~ contains polyfill

--- js/app_lib.js
+++ js/app_lib.js
@@ -1737,15 +1737,43 @@ function searchalize(str) {
 //////////////
 // CONTAINS //
 //////////////
+if (![].contains) {
+  Object.defineProperty(Array.prototype, 'contains', {
+    enumerable: false,
+    configurable: true,
+    writable: true,
+    value: function(searchElement/*, fromIndex*/) {
+      if (this === undefined || this === null) {
+        throw new TypeError('Cannot convert this value to object');
+      }
+      var O = Object(this);
+      var len = parseInt(O.length) || 0;
+      if (len === 0) { return false; }
+      var n = parseInt(arguments[1]) || 0;
+      if (n >= len) { return false; }
+      var k;
+      if (n >= 0) {
+        k = n;
+      } else {
+        k = len + n;
+        if (k < 0) k = 0;
+      }
+      while (k < len) {
+        var currentElement = O[k];
+        if (searchElement === currentElement ||
+            searchElement !== searchElement && currentElement !== currentElement
+        ) {
+          return true;
+        }
+        k++;
+      }
+      return false;
+    }
+  });
+}
 String.prototype.contains = function() {
 	return String.prototype.indexOf.apply( this, arguments ) !== -1;
 };
-Array.prototype.contains = function(obj) {
-	return JSON.stringify(this).indexOf(obj) > -1;
-};
-//String.prototype.contains = function(obj) {
-//	return this.indexOf(obj) > -1;
-//};
 ////////////////
 // SORTBYATTR //
 ////////////////
