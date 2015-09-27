3c85e274c757efafff0463cd5cedfa7922eb2c35
André Cancian <cancian@ateus.net>
2015-09-27 10:23:16 -0300

    1.9.9 ~ cors generic

--- cors.php
+++ cors.php
@@ -1,22 +1,23 @@
 <?php ###########
 ## ENABLE CORS ##
 #################
-header('cache-control: no-cache');
 header('Content-Type: text/html; charset=utf-8');
-#header('Access-Control-Allow-Origin: *');
-#header('Access-Control-Allow-Methods: GET, POST');
-#header('Access-Control-Allow-Headers: X-Requested-With');
+header('cache-control: no-cache');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true ');
+header('Access-Control-Allow-Methods: OPTIONS, GET, POST');
+header('Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control');
 /////////
 // W3C //
 /////////
 // Allow from any origin
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-	header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-	header('Access-Control-Allow-Credentials: true');
-}
+#if (isset($_SERVER['HTTP_ORIGIN'])) {
+#	header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+#	header('Access-Control-Allow-Credentials: true');
+#}
 // Access-Control headers are received during OPTIONS requests
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))  { header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); }
-	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) { header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}"); }
-}
+#if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+#	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))  { header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); }
+#	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) { header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}"); }
+#}
 ?>
