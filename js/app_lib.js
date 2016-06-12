643e8db928d25f0f9d427c955bd94baffbb559fd
André Cancian <cancian@ateus.net>
2016-06-12 08:50:08 -0300

    1.9.17 ~ str_replace function

--- js/app_lib.js
+++ js/app_lib.js
@@ -1697,6 +1697,58 @@ function trimDot(str) {
 	if(!str.length) { return ''; }
 		return str.replace(/\.$/, '').replace(/\,$/, '');
 }
+/////////////////
+// STR_REPLACE //
+/////////////////
+function str_replace(search, replace, subject, countObj) {
+	var i = 0
+		var j = 0
+		var temp = ''
+		var repl = ''
+		var sl = 0
+		var fl = 0
+		var f = [].concat(search)
+		var r = [].concat(replace)
+		var s = subject
+		var ra = Object.prototype.toString.call(r) === '[object Array]'
+		var sa = Object.prototype.toString.call(s) === '[object Array]'
+		s = [].concat(s)
+
+		var $global = (typeof window !== 'undefined' ? window : GLOBAL)
+		$global.$locutus = $global.$locutus || {}
+	var $locutus = $global.$locutus
+		$locutus.php = $locutus.php || {}
+
+	if (typeof(search) === 'object' && typeof(replace) === 'string') {
+		temp = replace
+			replace = []
+			for (i = 0; i < search.length; i += 1) {
+				replace[i] = temp
+			}
+			temp = ''
+			r = [].concat(replace)
+			ra = Object.prototype.toString.call(r) === '[object Array]'
+	}
+
+	if (typeof countObj !== 'undefined') {
+		countObj.value = 0
+	}
+
+	for (i = 0, sl = s.length; i < sl; i++) {
+		if (s[i] === '') {
+			continue
+		}
+		for (j = 0, fl = f.length; j < fl; j++) {
+			temp = s[i] + ''
+				repl = ra ? (r[j] !== undefined ? r[j] : '') : r[0]
+				s[i] = (temp).split(f[j]).join(repl)
+				if (typeof countObj !== 'undefined') {
+					countObj.value += ((temp.split(f[j])).length - 1)
+				}
+		}
+	}
+	return sa ? s : s[0]
+}
 ///////////////
 // ISEMPTY() //
 ///////////////
@@ -2112,6 +2164,7 @@ function dayFormat(input) {
 function dateDiff(date1,date2) {
 	//no future dates
 	//if(date1 > date2) { date1 = new Date().getTime(); }
+	if(!LANG) { return; }
 
 	//Get 1 day in milliseconds
 	var one_day  = 1000*60*60*24;
