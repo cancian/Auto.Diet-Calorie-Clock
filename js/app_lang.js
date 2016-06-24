addde52e45b5b522c912b42eac2bb528fd90f85b
André Cancian <cancian@ateus.net>
2016-06-24 01:41:15 -0300

    1.9.18 ~ jquery 2.2.4 again

--- js/app_lang.js
+++ js/app_lang.js
@@ -2,7 +2,7 @@
 // LANGUAGE ENGINE //
 /////////////////////
 var appName = 'KCals';
-var appVersion = '1.9.18 (191805)';
+var appVersion = '1.9.18 (191806)';
 var appBuild = appVersion.split(' ')[1].replace('(', '').replace(')', ''); //appVersion.slice(7,-1);
 var appRelease = appVersion.split(' ')[0];
 var reviewMode = false;
