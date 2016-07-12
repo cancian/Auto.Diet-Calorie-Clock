60fc801705550c904e150cfe6f70543c7041b512
André Cancian <cancian@ateus.net>
2016-07-12 05:16:06 -0300

    2.0 ~ localforage ~ fix mail.php escape new line

--- mail.php
+++ mail.php
@@ -5,7 +5,7 @@
 #######################
 if(strlen($_REQUEST['msg']) > 0) {
 	mail("cancian@kcals.net", "« KCals » Suggestion Box (".$_REQUEST['usr'].")","E-mail: " . htmlspecialchars($_REQUEST['mail']) . "<br /><br />Message: <br />" . htmlspecialchars($_REQUEST['msg']),
-	"From:" . $_REQUEST['mail'] . "r\n"
+	"From:" . $_REQUEST['mail'] . "\r\n"
 	."Content-type: text/html; charset=utf-8\r\n"
 	."Reply-To:" . $_REQUEST['mail'] . "\r\n"
 	."X-Mailer: PHP/" . phpversion()
