0172f3d72ea5717a846d31f5a5448c31957b3076
André Cancian <cancian@ateus.net>
2016-07-26 17:55:41 -0300

    2.0.1 ~ cordova msapp 4.4.1

--- js/app_lang.js
+++ js/app_lang.js
@@ -2,10 +2,10 @@
 // LANGUAGE ENGINE //
 /////////////////////
 var appName = 'ChronoBurn';
-var appVersion = '2.0.1 (200100)';
+var appVersion = '2.0.1 (200101)';
 var appBuild = appVersion.split(' ')[1].replace('(', '').replace(')', ''); //appVersion.slice(7,-1);
 var appRelease = appVersion.split(' ')[0];
-var reviewMode = true;
+var reviewMode = false;
 var langArray = /en|pt|ar|bg|cs|da|de|el|es|fi|fr|hu|it|id|in|ja|ko|nl|no|nb|pl|ro|ru|sv|tr|vi|zh|zt/;
 var lang = 'en';
 var defaultLang = 'en';
