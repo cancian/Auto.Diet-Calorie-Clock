2c767575b586dc8e3af6e9340ec3341a24ed841c
André Cancian <cancian@ateus.net>
2016-12-10 10:06:41 -0200

    2.1.1 ~ desc.txt add test heading

--- js/app_lang.js
+++ js/app_lang.js
@@ -1,6 +1,7 @@
 ﻿/////////////////////
 // LANGUAGE ENGINE //
 /////////////////////
+//OPTM - my,fitness,pal,fat,secret,fit,bit,run,keeper,plate,bmi,kg,lose,it,calorie,counter,tracker,diary,burn
 //OPT  - my,fitness,pal,fat,secret,fit,bit,run,keeper,plate,web,md,lose,it,calorie,counter,tracker,diary,burn
 //W10  - calorie counter, weight loss, lose weight, burn fat, weight tracker, kcals, myfitnesspal
 //IOS  - calorie,counter,weight,loss,lose,burn,fat,tracker,bmi,calculator,fit,cycling,myfitnesspal,healthbook
