386578f64160c7fa33682313176e2343057ba244
André Cancian <cancian@ateus.net>
2017-01-14 08:21:41 -0200

    2.1.4 ~ static.js headerInfoHandler

--- js/app_static.js
+++ js/app_static.js
@@ -1137,7 +1137,8 @@ if(app.is.scrollable) {
 	/////////////////////////////////
 	// HEADER INFO ICON XY HANDLER //
 	/////////////////////////////////
-	$('#appHeader').on(tap + app.device.android ? ' click' : '', function(evt) {
+	var headerInfoHandler = app.device.android ? 'click ' + tap : tap;
+	$('#appHeader').on(headerInfoHandler, function(evt) {
 		//HANDLE WINDOW
 		var xTap = parseInt(app.pointer(evt).x);
 		if(xTap < 125 && xTap > 0) {
@@ -1154,18 +1155,10 @@ if(app.is.scrollable) {
 			if($('#pageSlideFood').length)				{ return; }
 			if($('#appHeader').hasClass('blockInfo'))	{ return; }
 			if($('#timerDailyInput').is(':focus'))		{ return; }
-			if($('#timerDailyInput').is(':animated'))	{ return; }
 			//COLOR ANIMATION VALUE
-			if(Number($('#timerDailyInput').css2('color').split('rgba(255, 255, 255, ').join('').split(')').join('')) > 0.8) { return; }
+			if(parseFloat($('#timerDailyInput').css2('color').split('rgba(255, 255, 255, ').join('').split(')').join('')) > 0.8) { return; }
 			//INFO WINDOW
-			getNewWindow('Getting used to real-time','<div id="blockInfo">' + LANG.HELP_TOPICS_ARRAY['en']['Getting used to real-time'] + '</div>',function() {
-				//$('#tabHelp').removeClass('hidden');
-				//app.handlers.activeRow('#openHelp','button',function(evt) {
-				//	appFooter('tab4', 0, function() {
-				//		buildHelpMenu('direct');
-				//	});
-				//});
-			});
+			getNewWindow('Getting used to real-time','<div id="blockInfo">' + LANG.HELP_TOPICS_ARRAY['en']['Getting used to real-time'] + '</div>');
 		}
 	});
 	//////////////////////
