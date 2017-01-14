7d18357f1930e63dcc088a482b83b1744212ce1b
André Cancian <cancian@ateus.net>
2017-01-14 05:20:37 -0200

    2.1.4 ~ version bump

--- js/app_lang.js
+++ js/app_lang.js
@@ -27,10 +27,10 @@
 // myfitnesspal 10,100,000
 //////////////////////////
 var appName = 'ChronoBurn';
-var appVersion = '2.1.3 (210304)';
+var appVersion = '2.1.4 (210400)';
 var appBuild = appVersion.split(' ')[1].replace('(', '').replace(')', ''); //appVersion.slice(7,-1);
 var appRelease = appVersion.split(' ')[0];
-var reviewMode = true;
+var reviewMode = false;
 var langArray = /en|pt|ar|bg|cs|da|de|el|es|fi|fr|hu|it|id|in|ja|ko|nl|no|nb|pl|ro|ru|sv|tr|vi|zh|zt/;
 var lang = 'en';
 var defaultLang = 'en';
