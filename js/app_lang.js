b1c54901459510b92a909730e78feaa2bbe6ce68
André Cancian <cancian@ateus.net>
2018-06-01 12:44:07 -0300

    2.4 ~ version bump

--- js/app_lang.js
+++ js/app_lang.js
@@ -2,7 +2,7 @@
 // LANGUAGE ENGINE //
 /////////////////////
 var appName = 'ChronoBurn';
-var appVersion = '2.3.4 (234003)';
+var appVersion = '2.4 (240000)';
 var appBuild   = appVersion.split(' ')[1].replace('(', '').replace(')', ''); //appVersion.slice(7,-1);
 var appRelease = appVersion.split(' ')[0];
 var reviewMode = true;
