85c0d0481c2b61e4fc1aaebb7e909b53de31e682
André Cancian <cancian@ateus.net>
2018-06-02 17:32:34 -0300

    2.3.4 ~ fbpixel

--- js/app_static.js
+++ js/app_static.js
@@ -168,6 +168,30 @@ $('head').append2('<style type="text/css" id="cssAutoUpdate">\
 	.spinnerMask.updtdb #loadMask:before    { content: "' + LANG.UPDATING_DB[lang]     + '"; }\
 	.spinnerMask.newwindow #loadMask:before { content: "' + LANG.LOADING[lang]         + '"; }\
 </style>');
+/////////////////////////
+// FACEBOOK PIXEL CODE //
+/////////////////////////
+if(app.device.ios) {
+$('head').append2("\
+<!-- Facebook Pixel Code -->\
+<script>\
+  !function(f,b,e,v,n,t,s)\
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?\
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};\
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';\
+  n.queue=[];t=b.createElement(e);t.async=!0;\
+  t.src=v;s=b.getElementsByTagName(e)[0];\
+  s.parentNode.insertBefore(t,s)}(window, document,'script',\
+  'https://connect.facebook.net/en_US/fbevents.js');\
+  fbq('init', '1015308341967347');\
+  fbq('track', 'PageView');\
+</script>\
+<noscript><img height='1' width='1' style='display:none'\
+  src='https://www.facebook.com/tr?id=1015308341967347&ev=PageView&noscript=1'\
+/></noscript>\
+<!-- End Facebook Pixel Code -->');");
+}
+///////////////////
 updateNutriRatio();
 updateEntriesSum();
 ///////////////
