fa23197d9f0907c643a7d0b1b032bbb0e625eccf
André Cancian <cancian@ateus.net>
2018-07-10 15:28:30 -0300

    2.4.4 ~ ios cloud data ~ rewww ~ version bump txt

--- res/rewww.bat
+++ res/rewww.bat
@@ -226,11 +226,11 @@ cd k:\www\res\icon\ios\AppIcon.appiconset
 copy *.* k:\platforms\ios\kcals\images.xcassets\AppIcon.appiconset
  
 rem cd\ 
-deltree /y /z /s /q /t "k:\platforms\ios\KCals WatchKit App\Images.xcassets\WatchIcon.appiconset"
-rm -r "k:\platforms\ios\KCals WatchKit App\Images.xcassets\WatchIcon.appiconset" 
-mkdir "k:\platforms\ios\KCals WatchKit App\Images.xcassets\WatchIcon.appiconset" 
-cd K:\www\res\icon\ios\WatchIcon.appiconset
-copy *.* "k:\platforms\ios\KCals WatchKit App\Images.xcassets\WatchIcon.appiconset"
+rem deltree /y /z /s /q /t "k:\platforms\ios\KCals WatchKit App\Images.xcassets\WatchIcon.appiconset"
+rem rm -r "k:\platforms\ios\KCals WatchKit App\Images.xcassets\WatchIcon.appiconset" 
+rem mkdir "k:\platforms\ios\KCals WatchKit App\Images.xcassets\WatchIcon.appiconset" 
+rem cd K:\www\res\icon\ios\WatchIcon.appiconset
+rem copy *.* "k:\platforms\ios\KCals WatchKit App\Images.xcassets\WatchIcon.appiconset"
  
 rem cd\ 
 deltree /y /z /s /q /t k:\platforms\osx\kcals\images.xcassets\AppIcon.appiconset 
