725e3de6a6e9fa84b283ea6ee76af7b12d6058af
André Cancian <cancian@ateus.net>
2019-02-01 00:39:36 -0200

    2.5.2 ~ cors.php headers updated for XSS protection

--- cors.php
+++ cors.php
@@ -1,8 +1,26 @@
 <?php ###########
 ## ENABLE CORS ##
 #################
-header('Content-Type: text/html; charset=utf-8');
-header('cache-control: no-cache');
+##//header('cache-control: no-cache');
+##//header('Content-Type: text/html; charset=utf-8');
+
+$headers_list = headers_list();
+if (!in_array("Content-Type", $headers_list))				{ header("Content-Type: text/html; charset=utf-8"); }
+if (!in_array("cache-control", $headers_list))				{ header("cache-control: no-cache"); }
+if (!in_array("X-XSS-Protection", $headers_list))			{ header("X-XSS-Protection: 1; mode=block"); }
+if (!in_array("X-Content-Type-Options", $headers_list))		{ header("X-Content-Type-Options: nosniff"); }
+if (!in_array("X-Frame-Options", $headers_list))			{ header("X-Frame-Options: SAMEORIGIN"); }
+if (!in_array("Strict-Transport-Security", $headers_list))	{ header("Strict-Transport-Security: max-age=2592000; includeSubDomains"); }
+if (!in_array("Content-Security-Policy", $headers_list))	{ header("Content-Security-Policy: upgrade-insecure-requests"); }
+if (!in_array("Referrer-Policy", $headers_list))			{ header("Referrer-Policy: strict-origin-when-cross-origin"); }
+headers_list();
+
+####################################
+####################################
+####################################
+
+//header('Content-Type: text/html; charset=utf-8');
+//header('cache-control: no-cache');
 #header('Access-Control-Allow-Origin: *');
 #header('Access-Control-Allow-Credentials: true ');
 #header('Access-Control-Allow-Methods: OPTIONS, GET, POST');
