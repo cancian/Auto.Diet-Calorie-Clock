e2ff02f90e9aaa5f83a78bb22d82cd64b06d8814
cancian <cancian@ateus.net>
2019-02-08 23:13:27 -0200

    3.0 ~ version bump

--- js/app_lang.js
+++ js/app_lang.js
@@ -2,7 +2,7 @@
 // LANGUAGE ENGINE //
 /////////////////////
 var appName = 'Diet Clock';
-var appVersion = '3.0 (300001)';
+var appVersion = '3.0 (300002)';
 var appBuild   = appVersion.split(' ')[1].replace('(', '').replace(')', ''); //appVersion.slice(7,-1);
 var appRelease = appVersion.split(' ')[0];
 var reviewMode = true; // true false
