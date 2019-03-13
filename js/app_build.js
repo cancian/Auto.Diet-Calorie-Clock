a6cbc5383d119c872972bd29de3d89e9d5aa376d
cancian <cancian@ateus.net>
2019-03-13 08:04:28 -0300

    3.0.1 ~ redirect to app main page

--- js/app_build.js
+++ js/app_build.js
@@ -101,7 +101,7 @@ app.tab.settings = function(keepOpen) {
 		// HANDLER //
 		/////////////
 		var aboutHandler = function() {
-			app.handlers.activeRow('#b000','button',function() { app.url('https://dietclock.net/help.html'); });
+			app.handlers.activeRow('#b000','button',function() { app.url('https://dietclock.net'); });
 			app.handlers.activeRow('#b100','button',function() { app.url('android');    });
 			app.handlers.activeRow('#b200','button',function() { app.url('ios');        });
 			app.handlers.activeRow('#b300','button',function() { app.url('wp8');        });
