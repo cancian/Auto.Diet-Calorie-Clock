014aa9afd231d72e0e2f7338d25cf2944fb26119
cancian <cancian@ateus.net>
2019-10-09 18:24:59 -0300

    3.0.5 ~ update to jquery 2.2.4

--- js/app_lang.js
+++ js/app_lang.js
@@ -2,7 +2,7 @@
 // LANGUAGE ENGINE //
 /////////////////////
 var appName = 'Scientific Diet Clock';
-var appVersion = '3.0.5 (305002)';
+var appVersion = '3.0.5 (305005)';
 var appBuild   = appVersion.split(' ')[1].replace('(', '').replace(')', ''); //appVersion.slice(7,-1);
 var appRelease = appVersion.split(' ')[0];
 var reviewMode = true; // true false
